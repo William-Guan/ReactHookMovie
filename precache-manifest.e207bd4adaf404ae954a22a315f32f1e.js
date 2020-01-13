756f4242d88552d059eebedec3ec4151daf765dc
yanbinkwan <ybguan@ronglian.com>
2020-01-13 15:17:09 +0800

    Updates

---
+++ precache-manifest.e207bd4adaf404ae954a22a315f32f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55112121bda1810ec3ff2785b3ffd15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a69faf9895b9316fcf6f",
+    "url": "./static/css/main.922c05db.chunk.css"
+  },
+  {
+    "revision": "5b9dace3b64890938c84",
+    "url": "./static/js/2.2752746d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.2752746d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a69faf9895b9316fcf6f",
+    "url": "./static/js/main.f98d557d.chunk.js"
+  },
+  {
+    "revision": "3ee76d499c825375f5c5",
+    "url": "./static/js/runtime-main.e8820e2a.js"
+  }
+]);
